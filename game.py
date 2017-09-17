677e368bff69f657064df204f52302ed4b7c05fe
premus <premusvitka@gmail.com>
2017-09-17 04:34:58 -0400

    clean game.py

--- game.py
+++ game.py
@@ -16,78 +16,75 @@ class Game:
         '''(Game, str) -> NoneType
         Create a new Game given the name of an initial room
         to load. mapname excludes extension.'''
-
         self.current_room = self.load(mapname)
         self.hero = Hero(str(hero))
-        
-        # start hero in position 4 (center)
+        # start hero in position 4 (CENTER)
         # corresponding to Room.locations[4]
         self.current_room.add_hero(self.hero, CENTRE)
+
 
     def game_over(self):
         '''(Game) -> NoneType
         Return True iff hero's hit points are 0 or less.'''
-
         return self.hero.hp <= 0
+
 
     def load(self, mapname):
         '''(Game, str) -> Room
         Append .map to mapname and open corresponding file.
         Create a new Room with appropriately placed walls.
         Precondition: file is a valid map format file.'''
-
         if mapname == "None":
             return
         mapfile = open(mapname + ".map", "r")
+        #Lists for all the different Tiles
         walls = []
-        print(walls)
         items = []
         monsters = []
         doors = []
-        
+
+        #Parse the file and load the Lists
         row = 0
         assert mapfile.readline() == "MAPSTART\n"
-        currline = mapfile.readline()
-        while currline != "MAPFINISH\n":
-            for col in range(len(currline)):
-                if currline[col] == 'X':
+        current_line = mapfile.readline()
+        while current_line != "MAPFINISH\n":
+            for col in range(len(current_line)):
+                #If Tile is a Wall add it to walls list
+                if current_line[col] == 'X':
                     walls.append((row, col))
-                if currline[col] == 'D':
+                #If Tile is a Door add it to doors list   
+                if current_line[col] == 'D':
                     doors.append((row, col))
-            currline = mapfile.readline()
-            row += 1
+            current_line = mapfile.readline()
+            row = row + 1
 
-        #begin populating with items
+        #Begin populating items List with items
         assert mapfile.readline() == "ITEMS\n"
-        currline = mapfile.readline()
-        while currline != "MONSTERS\n":
+        current_line = mapfile.readline()
+        while current_line != "MONSTERS\n":
             # ADD ITEM PARSING CODE HERE
-            bob = currline.strip()
-            bob = bob.split(',')
-            items.append(bob)
-            currline = mapfile.readline()
+            strip_line = current_line.strip()
+            split_list = strip_line.split(',')
+            items.append(split_list)
+            current_line = mapfile.readline()
             
-        #begin populating with monsters
-        #assert mapfile.readline() == "MONSTERS\n"
-        currline = mapfile.readline()
-        while currline != "ENDFILE":
-            joe = currline.strip()
-            joe = joe.split(',')
-            monsters.append(joe)
-            currline = mapfile.readline()
+        #Begin populating monsters List with monsters
+        current_line = mapfile.readline()
+        while current_line != "ENDFILE":
+            strip_line = current_line.strip()
+            split_list = strip_line.split(',')
+            monsters.append(split_list)
+            current_line = mapfile.readline()
         mapfile.close()
         
-        #create Room
+        #Create Room
         room = Room(self, walls, items, monsters, doors, mapname)
-
-        # PROCESS .links FILES HERE
-
         return room
+
 
     def move_hero(self, x, y):
         '''(Game, int, int)
         Send a move command from the GameScreen to the current room
         to move the hero x tiles to the right and y tiles down.'''
-
         self.current_room.move_hero(x, y)
 
